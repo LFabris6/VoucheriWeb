9f0b595e104ae04d122808ac037fd780c9f5aabf
Leonard Fabris <leonardfabris3@gmail.com>
2022-04-29 23:53:27 +0200

    mp

--- web/voucher.py
+++ web/voucher.py
@@ -6,10 +6,13 @@ from mailmerge import MailMerge
 from PyPDF2 import PdfFileMerger
 import comtypes.client
 from django.contrib import messages
+from docx2pdf import convert
+import docx2pdf
 
 template = "static/api/voucher.docx"
 
 def create(od, do, sheet, request):
+    print(docx2pdf.__version__)
     try:
         gc = gspread.service_account(filename='static/api/client.json')
         sh = gc.open_by_key('1A41ogDqUWtHUALXmBpjP8Sj_ZRN8mq9c7t43LjYm2x4')
@@ -17,31 +20,15 @@ def create(od, do, sheet, request):
     except:
         return -1
 
-
+    pdfslist = PdfFileMerger()
     for i in range(od, do+1):
         ime = worksheet.cell(i, 1).value
         prezime = worksheet.cell(i, 2).value
         broj = worksheet.cell(i, 3).value
         gmail =  worksheet.cell(i, 4).value
         datum =  worksheet.cell(i, 5).value
-        ispuni(ime, prezime, broj, gmail, datum)
+        ispuni(ime, prezime, broj, gmail, datum, pdfslist)
     
-    comtypes.CoInitialize()
-    word = comtypes.client.CreateObject('Word.Application')
-    pdfslist = PdfFileMerger()
-    x = 0
-
-    directory = 'D:/Pro/mp/adriaticweb/static/voucheri/'
-
-    for filename in os.listdir(directory):
-        print(filename)
-        f = os.path.join(directory, filename)
-        output_file = docx_to_pdf(word, f, x)
-        pdfslist.append(open(output_file, 'rb'))
-        x += 1
-        
-    word.Quit()
-
     with open("static/finals/final.pdf", "wb") as result_pdf:
         pdfslist.write(result_pdf)
         pdfslist.close()
@@ -50,20 +37,18 @@ def create(od, do, sheet, request):
     ocisti()
    
     return 0
-def docx_to_pdf(word, file, x):
-    print(file)
-    input_file = os.path.abspath(file)
-    output_file = os.path.abspath("demo" + str(x) + ".pdf")
-    doc = word.Documents.Open(input_file)
-    doc.SaveAs(output_file, FileFormat=16+1)
-    doc.Close() 
-    return output_file
-def ispuni(ime, prezime, broj, gmail,datum):
+
+
+def ispuni(ime, prezime, broj, gmail,datum, pdfslist):
     print("kreiramo voucher za" + ime)
     document = MailMerge(template)
     document.merge(Ime=ime, prezime=prezime, email=gmail, broj = broj, rodenje=datum)
     path = "static/voucheri/voucher" + str(broj) + ".docx"
+    output_file = "static/voucheri/voucher" + str(broj) + ".pdf"
     document.write(path)
+    convert(path)
+    pdfslist.append(open(output_file, 'rb'))
+
 def ocisti():
     dir = "static/voucheri/"
     with os.scandir(dir) as entries:
