71e1ec4e66324032cb8181284d1cabef813b1af9
Leonard Fabris <83458396+LFabris6@users.noreply.github.com>
2022-07-18 11:58:00 +0200

    Update settings.py

--- adriaticweb/settings.py
+++ adriaticweb/settings.py
@@ -27,7 +27,26 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "web",
+    "pwa",
 ]
+
+PWA_APP_NAME = 'My App'
+PWA_APP_DESCRIPTION = "My app description" 
+PWA_APP_THEME_COLOR = '#0A0302' 
+PWA_APP_BACKGROUND_COLOR = '#ffffff' 
+PWA_APP_DISPLAY = 'standalone' 
+PWA_APP_SCOPE = '/' 
+PWA_APP_ORIENTATION = 'any'
+PWA_APP_START_URL = '/'
+PWA_APP_STATUS_BAR_COLOR = 'default' 
+PWA_APP_ICONS = [ { 'src': '/static/img/mplogo.png', 'sizes': '180x180' } ] 
+PWA_APP_ICONS_APPLE = [ { 'src': '/static/img/mplogo.png', 'sizes': '180x180' } ] 
+PWA_APP_SPLASH_SCREEN = [ { 'src': '/static/img/mplogo.png', 'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)' } ] 
+PWA_APP_DIR = 'ltr' 
+PWA_APP_LANG = 'en-US'
+
+PWA_APP_DEBUG_MODE = False
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
