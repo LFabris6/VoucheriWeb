06d64818da2487bb4a4df54cbb068e54039a2cb8
Leonard Fabris <83458396+LFabris6@users.noreply.github.com>
2022-07-24 23:43:45 +0200

    Create firebase-messaging-sw.js

---
+++ web/templates/web/firebase-messaging-sw.js
@@ -0,0 +1,42 @@
+
+
+importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-messaging.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyB0zenvZnCkMVJNRXDrU-oSfSLos56B6iU",
+    authDomain: "dron-334213.firebaseapp.com",
+    projectId: "dron-334213",
+    storageBucket: "dron-334213.appspot.com",
+    messagingSenderId: "684334334704",
+    appId: "1:684334334704:web:1f11d7e9b5a28318aff835"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+// [END initialize_firebase_in_sw]
+
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// [START background_handler]
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
+  payload = payload.data;
+  const notificationTitle = payload.title;
+  const notificationOptions = {
+    body: payload.body,
+    icon: payload.icon_url,
+  };
+
+  self.addEventListener('notificationclick', function (event) {
+    event.notification.close();
+    clients.openWindow(payload.url);
+  });
+
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
+// [END background_handler]
