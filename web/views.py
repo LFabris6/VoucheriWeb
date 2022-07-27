1a966295bd73e21ff213405253197d1e45c25c47
Leonard Fabris <83458396+LFabris6@users.noreply.github.com>
2022-07-27 11:46:13 +0200

    Update views.py

--- web/views.py
+++ web/views.py
@@ -32,7 +32,7 @@ class ServiceWorkerView(View):
 def test(request):
 
     print(FCMDevice.objects.send_message(
-    Message(notification=Notification(title="Popravak klime", body="Popravi klimu u 545", image="https://cdn-icons-png.flaticon.com/512/3844/3844593.png")
+    Message(notification=Notification(title="Popravak klime", body="Popravi klimu u 545")
     )))
   
  
